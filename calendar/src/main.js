2241546208ea9522d43d6a139f36cb34a90ce7b8
LiucCheng <13530000312@163.com>
2018-11-19 19:31:38 +0800

    Update main.js

--- calendar/src/main.js
+++ calendar/src/main.js
@@ -2,9 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import Picker from '';
-// import Calendar from 'vue2-datepick';
-import Calendar from './modules/index';
+import Picker from '';
+import Calendar from 'vue2-datepick';
+// import Calendar from './modules/index';
 Vue.use(Calendar);
 
 // Vue.use(Picker);
